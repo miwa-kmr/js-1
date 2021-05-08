d5ec294309f3ff96d843164b0c147ab6ce8a3e04
kimura <63143094+miwa-kmr@users.noreply.github.com>
2021-05-08 09:28:38 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+let str = "表示"
+
+let strElem = document.getElementById('str');
+
+//HTMLに表示
+strElem.innerHTML = str;
+
+let firstname = '木村';
+let name = '美和';
+console.log(firstname + name); //木村美和
